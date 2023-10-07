e11637060b1cc9aab006a84e454f469370872f15
kalintas <keremkalinntas@gmail.com>
2023-10-07 21:51:14 +0300

    Add parallax effect to planets

--- reference-cards-parallax.js
+++ reference-cards-parallax.js
@@ -3,33 +3,30 @@ const images = document.querySelectorAll(".card__img");
 const backgrounds = document.querySelectorAll(".card__bg");
 const range = 40;
 
-// const calcValue = (a, b) => (((a * 100) / b) * (range / 100) -(range / 2)).toFixed(1);
 const calcValue = (a, b) => ((a / b) * range - range / 2).toFixed(1); // thanks @alice-mx
 
 let timeout;
-document.addEventListener(
-  "mousemove",
-  ({ x, y }) => {
-    if (timeout) {
-      window.cancelAnimationFrame(timeout);
-    }
+document.addEventListener("mousemove",
+    ({ x, y }) => {
+        if (timeout) {
+            window.cancelAnimationFrame(timeout);
+        }
 
-    timeout = window.requestAnimationFrame(() => {
-      const yValue = calcValue(y, window.innerHeight);
-      const xValue = calcValue(x, window.innerWidth);
+        timeout = window.requestAnimationFrame(() => {
 
-      cards.style.transform = `rotateX(${yValue}deg) rotateY(${xValue}deg)`;
+            const yValue = calcValue(y, window.innerHeight);
+            const xValue = calcValue(x, window.innerWidth);
 
-      [].forEach.call(images, (image) => {
-        image.style.transform = `translateX(${-xValue}px) translateY(${yValue}px)`;
-      });
+            cards.style.transform = `rotateX(${yValue}deg) rotateY(${xValue}deg)`;
 
-      [].forEach.call(backgrounds, (background) => {
-        background.style.backgroundPosition = `${xValue * 0.45}px ${
-          -yValue * 0.45
-        }px`;
-      });
-    });
-  },
-  false
+            images.forEach(image => {
+                image.style.transform = `translateX(${xValue}px) translateY(${yValue}px)`;
+            });
+
+            backgrounds.forEach(background => {
+                background.style.backgroundPosition = `${xValue * 0.45}px ${-yValue * 0.45}px`;
+            });
+        });
+    },
+    false
 );
